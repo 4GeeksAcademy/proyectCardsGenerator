72c75a7ef995c8c756b73d304d5f5ffd48fa4360
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-16 19:36:46 +0000

    fourth commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let rdmNum = Math.floor(Math.random() * num.length);
+  document.querySelector("#icon").innerHTML = num[rdmNum];
+
+  let rdmSuit = Math.floor(Math.random() * suit.length);
+  document.querySelector("#suitOne").innerHTML = suit[rdmSuit];
+  document.querySelector("#suitTwo").innerHTML = suit[rdmSuit];
+
+  document.getElementById("genCard").addEventListener("click", window.onload);
 };
